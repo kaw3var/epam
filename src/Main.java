f3b0d28074ea9b503f01adfc43a8c4724245838c
kaw3var <bat.zham@gmail.com>
2024-10-12 13:09:14 +0800

    vol1 done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import vol1.taskB.*;
 import vol2.*;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,35 +11,47 @@ public class Main {
 
         System.out.print("Введите размер массива: ");
         int size = scanner.nextInt();
-//        ArrayList<Integer> array = new ArrayList<>(size);
-//        for (int i = 0; i < size; i++) {
-//            int num = scanner.nextInt();
-//            array.add(num);
-//        }
-//
-//        SolutionB.isSimple(array);
-
-        scanner.nextLine();
-        String[] numbers = new String[size];
-        System.out.println("Введите числа: ");
+        List<Integer> array = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            numbers[i] = scanner.nextLine();
+            int num = scanner.nextInt();
+            array.add(num);
         }
 
-        SolutionA.NumberOperations.findShortestAndLongest(numbers);
-        SolutionA.NumberOperations.sortByLength(numbers, true);
-        SolutionA.NumberOperations.printNumbersByAverageLength(numbers);
-        SolutionA.NumberOperations.findNumberWithMinUniqueDigits(numbers);
-        SolutionA.NumberOperations.findNumbersWithOnlyEvenDigits(numbers);
-        SolutionA.NumberOperations.findNumberWithIncreasingDigits(numbers);
-        SolutionA.NumberOperations.findNumberWithUniqueDigits(numbers);
-        SolutionA.NumberOperations.findSecondPalindrome(numbers);
+//        SolutionB.evenAndOdd(array);
+//        SolutionB.minMax(array);
+//        SolutionB.dividedBy3Else9(array);
+//        SolutionB.dividedBy5And7(array);
+//        SolutionB.isSimple(array);
+//        SolutionB.uniqueThreeDigits(array);
+//        SolutionB.sortedArray(array);
+        SolutionB.frequencySort(array);
+        SolutionB.checkHappyNumbers(array);
+        SolutionB.checkPalindrome(array);
+        SolutionB.findHalfSum(array);
 
-        SolutionA.NumberOperations.findQuadraticRoots(1, -3, 2);
+        /** Глава 2 Вариант А*/
 
-        System.out.println("\nРазработчик: Жамбалов Батор Аюрович");
-        System.out.println("Дата получения задания: 01.10.2024");
-        System.out.println("Дата сдачи задания: " + new java.util.Date());
+//        scanner.nextLine();
+//        String[] numbers = new String[size];
+//        System.out.println("Введите числа: ");
+//        for (int i = 0; i < size; i++) {
+//            numbers[i] = scanner.nextLine();
+//        }
+//
+//        SolutionA.NumberOperations.findShortestAndLongest(numbers);
+//        SolutionA.NumberOperations.sortByLength(numbers, true);
+//        SolutionA.NumberOperations.printNumbersByAverageLength(numbers);
+//        SolutionA.NumberOperations.findNumberWithMinUniqueDigits(numbers);
+//        SolutionA.NumberOperations.findNumbersWithOnlyEvenDigits(numbers);
+//        SolutionA.NumberOperations.findNumberWithIncreasingDigits(numbers);
+//        SolutionA.NumberOperations.findNumberWithUniqueDigits(numbers);
+//        SolutionA.NumberOperations.findSecondPalindrome(numbers);
+//
+//        SolutionA.NumberOperations.findQuadraticRoots(1, -3, 2);
+//
+//        System.out.println("\nРазработчик: Жамбалов Батор Аюрович");
+//        System.out.println("Дата получения задания: 01.10.2024");
+//        System.out.println("Дата сдачи задания: " + new java.util.Date());
 
         scanner.close();
     }
--- src/vol1/taskB/SolutionB.java
+++ src/vol1/taskB/SolutionB.java
@@ -3,14 +3,15 @@ package vol1.taskB;
 import java.util.*;
 
 public class SolutionB {
-    public static void isEmpty(ArrayList<Integer> arr) {
+    public static void isEmpty(List<Integer> arr) {
         if (arr.isEmpty()) {
             System.out.println("Список пустой");
         }
     }
 
-    public static void evenAndOdd(ArrayList<Integer> arr) {
+    public static void evenAndOdd(List<Integer> arr) {
         isEmpty(arr);
+        System.out.println("Задание B1");
         for (int el : arr) {
             if (el % 2 == 0) {
                 System.out.println("Четное: " + el);
@@ -20,8 +21,9 @@ public class SolutionB {
         }
     }
 
-    public static void minMax(ArrayList<Integer> arr) {
+    public static void minMax(List<Integer> arr) {
         isEmpty(arr);
+        System.out.println("Задание B2");
         int max = arr.get(0);
         int min = arr.get(0);
         for (int el : arr) {
@@ -36,34 +38,34 @@ public class SolutionB {
         System.out.println("Максимальное значение: " + max);
     }
 
-    public static void dividedBy3Else9(ArrayList<Integer> arr) {
+    public static void dividedBy3Else9(List<Integer> arr) {
         isEmpty(arr);
+        System.out.println("Задание B3");
         for (int el : arr) {
             if (el % 3 == 0) {
-                System.out.print(el + " ");
+                System.out.println(el + " ");
             }
         }
-        System.out.println();
     }
 
-    public static void dividedBy5And7(ArrayList<Integer> arr) {
+    public static void dividedBy5And7(List<Integer> arr) {
         isEmpty(arr);
+        System.out.println("Задание B4");
         for (int el : arr) {
             if (el % 5 == 0 & el % 7 == 0) {
-                System.out.print(el + " ");
+                System.out.println(el + " ");
             }
         }
-        System.out.println();
     }
 
-    public static void isSimple(ArrayList<Integer> arr) {
+    public static void isSimple(List<Integer> arr) {
         isEmpty(arr);
+        System.out.println("Задание B5");
         for (int el : arr) {
             if (isPrime(el)) {
                 System.out.println(el + " ");
             }
         }
-        System.out.println();
     }
 
     public static boolean isPrime(int num) {
@@ -78,13 +80,13 @@ public class SolutionB {
         return true;
     }
 
-    public static void uniqueThreeDigits(ArrayList<Integer> arr) {
+    public static void uniqueThreeDigits(List<Integer> arr) {
+        System.out.println("Задание B6");
         for (int el : arr) {
             if (hasUniqueDigits(el)) {
                 System.out.println(el + " ");
             }
         }
-        System.out.println();
     }
 
     public static boolean hasUniqueDigits(int num) {
@@ -94,9 +96,10 @@ public class SolutionB {
         return hundred != ten && hundred != unit && ten != unit;
     }
 
-    public static void sortedArray(ArrayList<Integer> arr) {
+    public static void sortedArray(List<Integer> arr) {
         isEmpty(arr);
-        ArrayList<Integer> tempArr = new ArrayList<>();
+        System.out.println("Задание B7");
+        List<Integer> tempArr = new ArrayList<>();
         int size = arr.size();
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size - i - 1; j++) {
@@ -109,13 +112,14 @@ public class SolutionB {
         }
     }
 
-    public static void frequencySort(ArrayList<Integer> arr) {
+    public static void frequencySort(List<Integer> arr) {
         isEmpty(arr);
+        System.out.println("Задание В8");
         HashMap<Integer, Integer> freqMap = new HashMap<>();
         for (int num : arr) {
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
         }
-        ArrayList<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(freqMap.entrySet());
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(freqMap.entrySet());
         Collections.sort(entryList, new Comparator<Map.Entry<Integer, Integer>>() {
             @Override
             public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
@@ -127,9 +131,67 @@ public class SolutionB {
             }
         });
 
+
         System.out.println("Числа в порядке частоты убывания");
         for (Map.Entry<Integer, Integer> entry : entryList) {
             System.out.println("Число: " + entry.getKey() + ",\tЧастота: " +entry.getValue());
+        }
+    }
+
+    public static void checkHappyNumbers(List<Integer> numbers) {
+        System.out.println("Задание В9");
+        for (int number : numbers) {
+            if (isHappy(number)) {
+                System.out.println(number + " счастливое число.");
+            } else {
+                System.out.println(number + " не счастливое число.");
+            }
+        }
+    }
+
+    public static boolean isHappy(int n) {
+        HashSet<Integer> seenNumbers = new HashSet<>();
+        while (n != 1 && !seenNumbers.contains(n)) {
+            seenNumbers.add(n);
+            n = sumOfSquaresOfDigits(n);
+        }
+        return n == 1;
+    }
+
+    private static int sumOfSquaresOfDigits(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static void checkPalindrome(List<Integer> numbers) {
+        System.out.println("Задание В10");
+        for (int number : numbers) {
+            if (isPalindrome(number)) {
+                System.out.println(number + " палиндром.");
+            } else {
+                System.out.println(number + " не палиндром.");
+            }
+        }
+    }
+
+    public static boolean isPalindrome(int n) {
+        String original = Integer.toString(n);
+        String reversed = new StringBuilder(original).reverse().toString();
+        return original.equals(reversed);
+    }
+
+    public static void findHalfSum(List<Integer> numbers) {
+        System.out.println("Задание В11");
+        for (int i = 1; i < numbers.size() - 1; i++) {
+            int halfSum = (numbers.get(i - 1) + numbers.get(i + 1)) / 2;
+            if (numbers.get(i) == halfSum) {
+                System.out.println("Элемент " + numbers.get(i) + " равен полусумме соседних элементов.");
+            }
         }
     }
 }
